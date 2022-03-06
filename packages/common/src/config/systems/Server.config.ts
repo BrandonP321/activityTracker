1b8c9e19e7181ee5c0351be7c57f367cd8abcc88
Brandon Phillips <brandon.phillips@bphillips.dev>
2022-03-06 00:23:00 -0800

    Updated API url in master confgi

--- packages/common/src/config/systems/Server.config.ts
+++ packages/common/src/config/systems/Server.config.ts
@@ -3,8 +3,8 @@ import { ConfigParam as Param } from "../ConfigParam";
 
 export const ServerConfig = new System({ enabled: true, description: "Settings for back end server, makes entire API inaccessible if disabled" }, {
     ConnectionSettings: new System({ enabled: true, description: "Settings for connecting mobile & web clients to API" }, {
-        ClientAPIUrlString: new Param({ enabled: true, value: "asdf", overrides: {
-            live: { enabled: true, value: "live url" },
+        ClientAPIUrlString: new Param({ enabled: true, value: "http://localhost:8000", overrides: {
+            live: { enabled: false, value: "live url" },
             dev: { enabled: true, value: "dev url" },
             local: { enabled: true, value: "http://localhost:8000" },
         }})
